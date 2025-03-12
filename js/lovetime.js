96aceeee31cf0056150d1e6666eb78b2c729a372
HuangJIanming <jm.work@qq.com>
2025-03-12 11:18:41 +0800

    1

--- js/lovetime.js
+++ js/lovetime.js
@@ -13,7 +13,7 @@ function lovetime() {
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
     // 修改这里时间即可 我的是2018.11.19
-    var t1 = Date.UTC(2018, 11, 19, 00, 00, 00)
+    var t1 = Date.UTC(2025, 2, 27, 17, 55, 0)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
@@ -22,7 +22,7 @@ function lovetime() {
     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
     var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
         minutes) / seconds)
-    document.getElementById("lovetime").innerHTML = "我们已经在一起 " + diffYears + "年" + diffDays + "天" +
+    document.getElementById("lovetime").innerHTML = "我们已经相识 " + diffYears + "年" + diffDays + "天" +
         diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
 }
 lovetime()
--- js/talk.js
+++ js/talk.js
@@ -3,12 +3,12 @@ new TypeIt("#tqyxhj", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("TQY && XHJ")
+    .type("圣女 && 咖啡续命")
     .pause(2000)
     .delete(null, {
         delay: 500
     })
-    .type("一起走过的第二个520...")
+    .type("相识的第14天...")
     .pause(3000)
     .go();
 
